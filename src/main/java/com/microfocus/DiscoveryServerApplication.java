8f63335567bd7aee758f65d29646cb733d0545de
gurubbc <gurumurthy.ramamurthy@gmail.com>
2023-03-07 12:09:57 +0530

    Add files via upload

---
+++ src/main/java/com/microfocus/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.microfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+		System.out.println("Discovery service server started.....");
+	}
+
+}
